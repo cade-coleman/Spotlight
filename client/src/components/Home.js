6755516be391527421d4f9cdd1e92e394ecaae2e
Aileen Bryand <aileenale09@gmail.com>
2022-09-01 10:58:44 -0500

    components folder

---
+++ client/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div name='login' className='hidden bg-gray-300 w-full h-screen'>
+        {/*NAVBAR */}
+
+        <div>
+            <nav className='bg-[#0C0C0C] w-full h-[80px] text-gray-300'>
+
+            </nav>
+        </div>
+    </div>
+  )
+}
+
+
+export default Home;
