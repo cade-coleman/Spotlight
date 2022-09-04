2094699fa932d878e334bd1046e463ee30608c15
Cade Coleman <105037672+cade-coleman@users.noreply.github.com>
2022-09-03 20:19:31 -0500

    added more code

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import Account from "./pages/Account";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup"
+import EditProfile from "./pages/EditProfile";
+import Signup from "./pages/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <div className="container"></div>
         <Routes>
             <Route path="/account" element={<Account />} />
+            <Route path="/editProfile" element={<EditProfile/>} />
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
