07b328dad7056239301a6ebb4cfd243ea4968044
Cade Coleman <105037672+cade-coleman@users.noreply.github.com>
2022-09-05 17:19:09 -0500

    added to account page

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,13 @@ import {
   ApolloProvider,
   createHttpLink,
 } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import Account from "./pages/Account";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import EditProfile from "./pages/EditProfile";
 import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import EditProfile from "./pages/EditProfile";
 import Notifications from "./pages/Notifications";
+import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const httpLink = createHttpLink({
@@ -40,11 +40,11 @@ function App() {
         <div className="flex-column justify-flex-start min-100-vh">
           <div className="container"></div>
           <Routes>
-            <Route path="/account" element={<Account />} />
-            <Route path="/editProfile" element={<EditProfile />} />
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/editProfile" element={<EditProfile />} />
             <Route path="/notifications" element={<Notifications />} />
           </Routes>
         </div>
