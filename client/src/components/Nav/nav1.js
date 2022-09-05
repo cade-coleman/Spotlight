6fc1edb4c81472ae56e214fe25c78ffb87e5777a
Austinjm121 <austinjm121@gmail.com>
2022-09-04 22:11:00 -0500

    Re-formatted almost every single js file.

--- client/src/components/Nav/nav1.js
+++ client/src/components/Nav/nav1.js
@@ -1,4 +1,4 @@
-"use strict"; 
+"use strict";
 
 const body = document.body;
 const bgColorsBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
@@ -8,41 +8,38 @@ const menuBorder = menu.querySelector(".menu__border");
 let activeItem = menu.querySelector(".active");
 
 function clickItem(item, index) {
+  menu.style.removeProperty("--timeOut");
 
-    menu.style.removeProperty("--timeOut");
-    
-    if (activeItem == item) return;
-    
-    if (activeItem) {
-        activeItem.classList.remove("active");
-    }
+  if (activeItem == item) return;
 
-    
-    item.classList.add("active");
-    body.style.backgroundColor = bgColorsBody[index];
-    activeItem = item;
-    offsetMenuBorder(activeItem, menuBorder);
-    
-    
+  if (activeItem) {
+    activeItem.classList.remove("active");
+  }
+
+  item.classList.add("active");
+  body.style.backgroundColor = bgColorsBody[index];
+  activeItem = item;
+  offsetMenuBorder(activeItem, menuBorder);
 }
 
 function offsetMenuBorder(element, menuBorder) {
-
-    const offsetActiveItem = element.getBoundingClientRect();
-    const left = Math.floor(offsetActiveItem.left - menu.offsetLeft - (menuBorder.offsetWidth  - offsetActiveItem.width) / 2) +  "px";
-    menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
-
+  const offsetActiveItem = element.getBoundingClientRect();
+  const left =
+    Math.floor(
+      offsetActiveItem.left -
+        menu.offsetLeft -
+        (menuBorder.offsetWidth - offsetActiveItem.width) / 2
+    ) + "px";
+  menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
 }
 
 offsetMenuBorder(activeItem, menuBorder);
 
 menuItems.forEach((item, index) => {
-
-    item.addEventListener("click", () => clickItem(item, index));
-    
-})
+  item.addEventListener("click", () => clickItem(item, index));
+});
 
 window.addEventListener("resize", () => {
-    offsetMenuBorder(activeItem, menuBorder);
-    menu.style.setProperty("--timeOut", "none");
+  offsetMenuBorder(activeItem, menuBorder);
+  menu.style.setProperty("--timeOut", "none");
 });
