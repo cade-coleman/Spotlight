0c28a33a8767d9c27ebfb21080af0b95c3d5bdc2
Cade Coleman <105037672+cade-coleman@users.noreply.github.com>
2022-09-06 00:03:22 -0500

    added styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,16 +15,16 @@ const Home = () => {
   return (
     <div>
       <Header />
+          <Nav />
       <div className=" bg-gray-300 w-full h-screen">
         <div className="flex flex-col items-center justify-center">
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <PostList posts={posts} title="Some Feed for posts" />
+            <PostList posts={posts} title="" />
           )}
         </div>
       </div>
-      <Nav />
     </div>
   );
 };
