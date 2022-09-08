91cf9986f0b86e102917350d9e119735314098e3
Cade Coleman <105037672+cade-coleman@users.noreply.github.com>
2022-09-08 01:27:57 -0500

    stuff

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/spotlight',
+  process.env.MONGODB_URI || 'mongodb://localhost/spotlight',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
